04a4bca87c5c5182ba835fc55e85c7cd52a281ed
Babbaj <babbaj45@gmail.com>
2021-08-29 21:25:16 -0400

    implement parsing the entire record

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
-use nom::{IResult};
+use nom::{IResult, Parser};
 use nom::branch::alt;
 use nom::bytes::complete::{is_not, tag, take, take_until};
-use nom::character::complete::{space0};
+use nom::character::complete::{space0, anychar};
 use nom::combinator::{eof};
 use nom::error::{ErrorKind};
 use nom::sequence::{pair, delimited};
 use nom::character::complete;
 
-#[derive(Clone, Default, Debug, PartialEq)]
+#[derive(Clone, Copy, Default, Debug, PartialEq)]
  struct ConfiguredLimits {
      volts_hi_times_ten: u16,
      volts_lo_times_ten: u16,
@@ -19,13 +19,18 @@ use nom::character::complete;
      oil_lo: u16
  }
 
- #[derive(Clone, Debug)]
- enum HeaderRecordType<'a> {
-     U(&'a str),
-     A(ConfiguredLimits)
+ #[derive(Clone, Debug, PartialEq)]
+ enum HeaderRecord {
+     U(String),
+     A(ConfiguredLimits),
+     F(FuelFlowLimits),
+     T(Timestamp),
+     C(ConfigInfo),
+     D(FlightInfo),
+     L(LastHeaderRecord)
  }
 
- #[derive(Clone, Default, Debug, PartialEq)]
+ #[derive(Clone, Copy, Default, Debug, PartialEq)]
  struct FuelFlowLimits {
      empty: u16,
      full: u16,
@@ -34,7 +39,7 @@ use nom::character::complete;
      k_factor2: u16,
  }
 
- #[derive(Clone, Default, Debug, PartialEq)]
+ #[derive(Clone, Copy, Default, Debug, PartialEq)]
  struct Timestamp {
      month: u16,
      day: u16,
@@ -44,7 +49,7 @@ use nom::character::complete;
      unknown: u16,
  }
 
- #[derive(Clone, Default, Debug, PartialEq)]
+ #[derive(Clone, Copy, Default, Debug, PartialEq)]
  struct ConfigInfo {
      model_number: u16,
      feature_flags_lo: u16,
@@ -53,13 +58,13 @@ use nom::character::complete;
      firmware_version: u16,
  }
 
- #[derive(Clone, Default, Debug, PartialEq)]
+ #[derive(Clone, Copy, Default, Debug, PartialEq)]
  struct FlightInfo {
      flight_number: u16,
      length: u16
  }
 
- #[derive(Clone, Default, Debug, PartialEq)]
+ #[derive(Clone, Copy, Default, Debug, PartialEq)]
  struct LastHeaderRecord {
      unknown: u16
  }
@@ -82,12 +87,8 @@ macro_rules! comma_delim {
     };
 }
 
- fn tail_number_parser(i: &str) -> IResult<&str, (&str, &str)> {
-     let (i, header_record_type) = take(1usize)(i)?;
-     let (i, _) = tag(",")(i)?;
-     let (i, tail_number) = not_underscore(i)?;
-
-     Ok((i, (header_record_type, tail_number)))
+ fn tail_number_parser(i: &str) -> IResult<&str, &str> {
+     not_underscore(i)
  }
 
  fn configured_limits_parser(i: &str) -> IResult<&str, ConfiguredLimits> {
@@ -192,10 +193,10 @@ fn last_header_record_parser(i: &str) -> IResult<&str, LastHeaderRecord> {
     }))
 }
 
- fn header_record_parser(i: &str) -> IResult<&str, (&str, &str)> {
+ fn header_record_parser(i: &str) -> IResult<&str, (char, &str)> {
      let (i, _) = tag("$")(i)?;
-     let (i, header_record_type) = take(1usize)(i)?;
-     let (i, _) = take_until(",")(i)?;
+     let (i, header_record_type) = anychar(i)?;
+     let (i, _) = tag(",")(i)?;
      let (i, header_record) = take_until("*")(i)?;
      let (i, _) = tag("*")(i)?;
      let (i, checksum) = take(2usize)(i)?;
@@ -206,20 +207,34 @@ fn last_header_record_parser(i: &str) -> IResult<&str, LastHeaderRecord> {
      }
 
      if computed_checksum == from_hex(checksum).unwrap() {
-         println!("checksum is heckin valid")
+         println!("checksum is heckin valid") // TODO: return error if checksum fails
      }
 
      Ok((i, (header_record_type, header_record)))
  }
 
+fn parse_record(i: &str) -> IResult<&str, HeaderRecord> {
+    let (_, (record_type, data)) = header_record_parser(i)?;
+
+    use HeaderRecord::*;
+    match record_type {
+        'U' => tail_number_parser.map(|x| U(x.to_owned())).parse(data),
+        'A' => configured_limits_parser.map(|x| A(x)).parse(data),
+        'F' => fuel_flow_parser.map(|x| F(x)).parse(data),
+        'T' => timestamp_parser.map(|x| T(x)).parse(data),
+        'C' => config_info_parser.map(|x| C(x)).parse(data),
+        'D' => flight_info_parser.map(|x| D(x)).parse(data),
+        'L' => last_header_record_parser.map(|x| L(x)).parse(data),
+        _ => Err(nom::Err::Error(nom::error::Error::new(i, ErrorKind::NoneOf)))
+    }
+}
+
  #[test]
  fn test() {
-     assert_eq!(not_underscore("N51SW__"), Ok(("__", "N51SW")));
-     assert_eq!(not_underscore("__N51SW"), Err(nom::Err::Error(nom::error::Error::new("__N51SW", ErrorKind::IsNot))));
+     assert_eq!(tail_number_parser("N51SW__"), Ok(("__", "N51SW")));
+     assert_eq!(tail_number_parser("__N51SW"), Err(nom::Err::Error(nom::error::Error::new("__N51SW", ErrorKind::IsNot))));
 
-     assert_eq!(tail_number_parser("U,N51SW__"), Ok(("__", ("U", "N51SW"))));
-
-     assert_eq!(configured_limits_parser("155,130,400,415, 60,1650,220, 75"), Ok(("", ConfiguredLimits {
+     let config_limit_example = ConfiguredLimits {
          volts_hi_times_ten: 155,
          volts_lo_times_ten: 130,
          dif: 400,
@@ -228,41 +243,53 @@ fn last_header_record_parser(i: &str) -> IResult<&str, LastHeaderRecord> {
          tit: 1650,
          oil_hi: 220,
          oil_lo: 75
-     })));
+     };
+     assert_eq!(configured_limits_parser("155,130,400,415, 60,1650,220, 75"), Ok(("", config_limit_example)));
+     assert_eq!(parse_record("$A,155,130,400,415, 60,1650,220, 75*70"), Ok(("", HeaderRecord::A(config_limit_example))));
 
-     assert_eq!(fuel_flow_parser("0, 49, 22,3183,3183"), Ok(("", FuelFlowLimits {
+     let fuel_flow_example = FuelFlowLimits {
          empty: 0,
          full: 49,
          warning: 22,
          k_factor: 3183,
          k_factor2: 3183,
-     })));
+     };
+     assert_eq!(fuel_flow_parser("0, 49, 22,3183,3183"), Ok(("", fuel_flow_example)));
+     assert_eq!(parse_record("$F,0, 49, 22,3183,3183*57"), Ok(("", HeaderRecord::F(fuel_flow_example))));
 
-     assert_eq!(timestamp_parser("5,13, 5,23, 2, 2222"), Ok(("", Timestamp {
+     let timestamp_example = Timestamp {
          month: 5,
          day: 13,
          year: 5,
          hour: 23,
          minute: 2,
          unknown: 2222,
-     })));
+     };
+     assert_eq!(timestamp_parser("5,13, 5,23, 2, 2222"), Ok(("", timestamp_example)));
+     assert_eq!(parse_record("$T, 5,13, 5,23, 2, 2222*65"), Ok(("", HeaderRecord::T(timestamp_example))));
 
-     assert_eq!(config_info_parser("700,63741, 6193, 1552, 292"), Ok(("", ConfigInfo {
+     let config_info_example = ConfigInfo {
          model_number: 700,
          feature_flags_lo: 63741,
          feature_flags_hi: 6193,
          unknown_flags: 1552,
          firmware_version: 292,
-     })));
+     };
+     assert_eq!(config_info_parser("700,63741, 6193, 1552, 292"), Ok(("", config_info_example)));
+     assert_eq!(parse_record("$C, 700,63741, 6193, 1552, 292*58"), Ok(("", HeaderRecord::C(config_info_example))));
 
-     assert_eq!(flight_info_parser("227, 3979"), Ok(("", FlightInfo {
+     let flight_info_example = FlightInfo {
          flight_number: 227,
          length: 3979,
-     })));
+     };
+     assert_eq!(flight_info_parser("227, 3979"), Ok(("", flight_info_example)));
+     assert_eq!(parse_record("$D,  227, 3979*57"), Ok(("", HeaderRecord::D(flight_info_example))));
 
-     assert_eq!(last_header_record_parser("49"), Ok(("", LastHeaderRecord {
+     let last_header_record_example = LastHeaderRecord {
          unknown: 49,
-     })));
+     };
+     assert_eq!(last_header_record_parser("49"), Ok(("", last_header_record_example)));
+     assert_eq!(parse_record("$L, 49*4D"), Ok(("", HeaderRecord::L(last_header_record_example))));
  }
 
 fn main() {
