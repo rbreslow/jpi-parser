39e3e5bcaf632eb18259012d4d5f247cfa3fe3b1
Babbaj <babbaj45@gmail.com>
2021-08-31 16:24:06 -0400

    fix checksum check

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,11 @@ use nom::{IResult, Parser};
 use nom::branch::alt;
 use nom::bytes::complete::{is_not, tag, take, take_until};
 use nom::character::complete::{space0, anychar};
-use nom::combinator::{eof};
+use nom::combinator::{eof, map_res, all_consuming};
 use nom::error::{ErrorKind};
 use nom::sequence::{pair, delimited};
 use nom::character::complete;
+use std::ops::BitXor;
 
 #[derive(Clone, Copy, Default, Debug, PartialEq)]
  struct ConfiguredLimits {
@@ -77,14 +78,16 @@ use nom::character::complete;
      u8::from_str_radix(input, 16)
  }
 
-macro_rules! comma_delim {
-    ($parser:expr) => {
-        delimited(
-            space0, // possible spaces to the left
-            $parser, // the number
-            pair(space0, alt((tag(","), eof))) // possible spaces to the right followed by a comma or end of the string
-        )
-    };
+fn parse_hex2(input: &str) -> IResult<&str, u8> {
+    map_res(take(2usize), |s| u8::from_str_radix(s, 16))(input)
+}
+
+fn parse_short(i: &str) -> IResult<&str, u16> {
+    delimited(
+        space0, // possible spaces to the left
+        complete::u16, // the number
+        pair(space0, alt((tag(","), eof))) // possible spaces to the right followed by a comma or end of the string
+    )(i)
 }
 
  fn tail_number_parser(i: &str) -> IResult<&str, &str> {
@@ -92,16 +95,14 @@ macro_rules! comma_delim {
  }
 
  fn configured_limits_parser(i: &str) -> IResult<&str, ConfiguredLimits> {
-     let mut parse = comma_delim!(complete::u16);
-
-     let (i, volts_hi_times_ten) = parse(i)?;
-     let (i, volts_lo_times_ten) = parse(i)?;
-     let (i, dif) = parse(i)?;
-     let (i, cht) = parse(i)?;
-     let (i, cld) = parse(i)?;
-     let (i, tit) = parse(i)?;
-     let (i, oil_hi) = parse(i)?;
-     let (i, oil_lo) = parse(i)?;
+     let (i, volts_hi_times_ten) = parse_short(i)?;
+     let (i, volts_lo_times_ten) = parse_short(i)?;
+     let (i, dif) = parse_short(i)?;
+     let (i, cht) = parse_short(i)?;
+     let (i, cld) = parse_short(i)?;
+     let (i, tit) = parse_short(i)?;
+     let (i, oil_hi) = parse_short(i)?;
+     let (i, oil_lo) = parse_short(i)?;
 
      Ok((i, ConfiguredLimits {
          volts_hi_times_ten,
@@ -116,13 +117,11 @@ macro_rules! comma_delim {
  }
 
 fn fuel_flow_parser(i: &str) -> IResult<&str, FuelFlowLimits> {
-    let mut parse = comma_delim!(complete::u16);
-
-    let (i, empty) = parse(i)?;
-    let (i, full) = parse(i)?;
-    let (i, warning) = parse(i)?;
-    let (i, k_factor) = parse(i)?;
-    let (i, k_factor2) = parse(i)?;
+    let (i, empty) = parse_short(i)?;
+    let (i, full) = parse_short(i)?;
+    let (i, warning) = parse_short(i)?;
+    let (i, k_factor) = parse_short(i)?;
+    let (i, k_factor2) = parse_short(i)?;
 
     Ok((i, FuelFlowLimits {
         empty,
@@ -134,14 +133,12 @@ fn fuel_flow_parser(i: &str) -> IResult<&str, FuelFlowLimits> {
 }
 
 fn timestamp_parser(i: &str) -> IResult<&str, Timestamp> {
-    let mut parse = comma_delim!(complete::u16);
-
-    let (i, month) = parse(i)?;
-    let (i, day) = parse(i)?;
-    let (i, year) = parse(i)?;
-    let (i, hour) = parse(i)?;
-    let (i, minute) = parse(i)?;
-    let (i, unknown) = parse(i)?;
+    let (i, month) = parse_short(i)?;
+    let (i, day) = parse_short(i)?;
+    let (i, year) = parse_short(i)?;
+    let (i, hour) = parse_short(i)?;
+    let (i, minute) = parse_short(i)?;
+    let (i, unknown) = parse_short(i)?;
 
     Ok((i, Timestamp {
         month,
@@ -154,13 +151,11 @@ fn timestamp_parser(i: &str) -> IResult<&str, Timestamp> {
 }
 
 fn config_info_parser(i: &str) -> IResult<&str, ConfigInfo> {
-    let mut parse = comma_delim!(complete::u16);
-
-    let (i, model_number) = parse(i)?;
-    let (i, feature_flags_lo) = parse(i)?;
-    let (i, feature_flags_hi) = parse(i)?;
-    let (i, unknown_flags) = parse(i)?;
-    let (i, firmware_version) = parse(i)?;
+    let (i, model_number) = parse_short(i)?;
+    let (i, feature_flags_lo) = parse_short(i)?;
+    let (i, feature_flags_hi) = parse_short(i)?;
+    let (i, unknown_flags) = parse_short(i)?;
+    let (i, firmware_version) = parse_short(i)?;
 
     Ok((i, ConfigInfo {
         model_number,
@@ -172,10 +167,8 @@ fn config_info_parser(i: &str) -> IResult<&str, ConfigInfo> {
 }
 
 fn flight_info_parser(i: &str) -> IResult<&str, FlightInfo> {
-    let mut parse = comma_delim!(complete::u16);
-
-    let (i, flight_number) = parse(i)?;
-    let (i, length) = parse(i)?;
+    let (i, flight_number) = parse_short(i)?;
+    let (i, length) = parse_short(i)?;
 
     Ok((i, FlightInfo {
         flight_number,
@@ -184,37 +177,32 @@ fn flight_info_parser(i: &str) -> IResult<&str, FlightInfo> {
 }
 
 fn last_header_record_parser(i: &str) -> IResult<&str, LastHeaderRecord> {
-    let mut parse = comma_delim!(complete::u16);
-
-    let (i, unknown) = parse(i)?;
+    let (i, unknown) = parse_short(i)?;
 
     Ok((i, LastHeaderRecord {
         unknown,
     }))
 }
 
- fn header_record_parser(i: &str) -> IResult<&str, (char, &str)> {
-     let (i, _) = tag("$")(i)?;
-     let (i, header_record_type) = anychar(i)?;
-     let (i, _) = tag(",")(i)?;
-     let (i, header_record) = take_until("*")(i)?;
+ fn header_record_parser(line: &str) -> IResult<&str, (char, &str)> {
+     let (i, _) = tag("$")(line)?;
+     let (i, middle) = take_until("*")(i)?;
      let (i, _) = tag("*")(i)?;
-     let (i, checksum) = take(2usize)(i)?;
+     let (rest, checksum) = parse_hex2(i)?;
+     let (i, header_record_type) = anychar(middle)?;
+     let (header_record, _) = tag(",")(i)?;
 
-     let mut computed_checksum: u8 = 0;
-     for byte in header_record.bytes() {
-         computed_checksum ^= byte;
+     let computed_checksum = middle.bytes().fold(0u8, u8::bitxor);
+
+     if computed_checksum != checksum {
+         return Err(nom::Err::Failure(nom::error::Error::new(line, ErrorKind::Verify)))
      }
 
-     if computed_checksum == from_hex(checksum).unwrap() {
-         println!("checksum is heckin valid") // TODO: return error if checksum fails
-     }
-
-     Ok((i, (header_record_type, header_record)))
+     Ok((rest, (header_record_type, header_record)))
  }
 
 fn parse_record(i: &str) -> IResult<&str, HeaderRecord> {
-    let (_, (record_type, data)) = header_record_parser(i)?;
+    let (_, (record_type, data)) = all_consuming(header_record_parser)(i)?;
 
     use HeaderRecord::*;
     match record_type {
@@ -225,7 +213,7 @@ fn parse_record(i: &str) -> IResult<&str, HeaderRecord> {
         'C' => config_info_parser.map(|x| C(x)).parse(data),
         'D' => flight_info_parser.map(|x| D(x)).parse(data),
         'L' => last_header_record_parser.map(|x| L(x)).parse(data),
-        _ => Err(nom::Err::Error(nom::error::Error::new(i, ErrorKind::NoneOf)))
+        _ => Err(nom::Err::Failure(nom::error::Error::new(i, ErrorKind::NoneOf)))
     }
 }
 
